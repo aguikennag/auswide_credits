4e9fffba5496100408490d6b65f06c39f9bb1fb2
unknown <aguikenna@fiberswift.com>
2022-11-10 07:37:31 +0100

    ''

--- users/models.py
+++ users/models.py
@@ -1,5 +1,3 @@
-from email.policy import default
-from msilib.schema import Property
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.text import slugify
