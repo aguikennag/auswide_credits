0445271bf0ec891c4a193dc6fa7bf0e7966bb844
unknown <aguikenna@fiberswift.com>
2023-03-29 12:44:56 +0100

    'updated_domain

--- core/context.py
+++ core/context.py
@@ -4,7 +4,7 @@ def core(request) :
     context = {
         'site_name' : "Metros Capital Bank",
         'site_name_short' : "Metros Capital",
-        'site_mail' : "support@metroscapitalbank.com",
+        'site_mail' : "support@metrosfinance.com",
         'site_address' : "4th and 5th, brandon road, New york city"
 
     }
