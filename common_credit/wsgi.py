32970383966e60922809b4f914793445b633b4c8
unknown <aguikenna@fiberswift.com>
2023-07-17 22:24:17 +0100

    'initial'

--- common_credit/wsgi.py
+++ common_credit/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for metropol project.
+WSGI config for common_credit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metropol.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'common_credit.settings')
 
 application = get_wsgi_application()
--- core/context.py
+++ core/context.py
@@ -2,9 +2,9 @@
 
 def core(request) :
     context = {
-        'site_name' : "Metros Capital Bank",
-        'site_name_short' : "Metros Capital",
-        'site_mail' : "support@metrosfinance.com",
+        'site_name' : "Common Wealth Credit",
+        'site_name_short' : "Common Wealth Credit",
+        'site_mail' : "support@commonwealthscredit.com",
         'site_address' : "4th and 5th, brandon road, New york city"
 
     }
