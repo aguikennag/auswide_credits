0e7672c7c834d36ac5dbb08b4688b76e1e21f5dd
unknown <aguikenna@fiberswift.com>
2023-07-26 21:49:07 +0100

    ''

--- wallet/transaction.py
+++ wallet/transaction.py
@@ -213,12 +213,64 @@ class Transfer(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
 
-      
         account = request.user.wallet
         form = self.form_class
         return render(request, self.template_name, locals())
 
+    
     def post(self, request, *args, **kwargs):
+        start = time.time()
+        form = self.form_class(
+            user=self.request.user,
+            data=request.POST
+        )
+        if form.is_valid():
+           
+            error = None
+            acc_num = form.cleaned_data.get('account_number')
+            amount = form.cleaned_data.get('amount')
+            transact_type = form.cleaned_data.get('transfer_type')
+            # if internal
+            receipient = None
+
+            if transact_type == 'Internal Transfer':
+                receipient = get_user_model().objects.get(account_number=acc_num)
+
+            else:
+                iban = form.cleaned_data.get('iban')
+                bic = form.cleaned_data.get('bic')
+                swift_number = form.cleaned_data.get('swift_number')
+                # check if details is in our list,else give network error
+                error = None
+
+            acc_name, bank_name, country = None, None, None
+
+            transact = transaction_model.objects.create(
+                user=request.user,
+                amount=amount,
+                transaction_type='debit',
+                nature=form.cleaned_data['transfer_type'],
+                description=form.cleaned_data['description'],
+                charge=0.00,
+                status="pending",
+                status_message="Waiting for Transaction Pin Authorization",
+                receiver=receipient,
+                swift_number=form.cleaned_data.get('swift_number', None),
+                iban=form.cleaned_data.get('iban', None),
+                bic=form.cleaned_data.get('bic', None),
+                account_number=acc_num,
+                account_name=acc_name,
+                country=country,
+                bank_name=bank_name
+            )
+
+            return HttpResponseRedirect(reverse('complete-transaction', args=[transact.transaction_id]))
+
+        else:
+            return render(request, self.template_name, locals())
+
+    
+    """def post(self, request, *args, **kwargs):
         start = time.time()
         form = self.form_class(user=self.request.user, data=request.POST)
         if form.is_valid():
@@ -328,3 +380,4 @@ class Transfer(LoginRequiredMixin, View):
                 return HttpResponseRedirect(reverse('complete-transaction', args=[transact.transaction_id]))
         else:
             return render(request, self.template_name, locals())
+    """
