ca28572164ae58597effd9115868ab4bbe384720
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-08-21 19:48:40 +0100

    ''

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ export PSYCOPG_DEBUG=1
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+
+ 
 # Apply any outstanding database migrations
 python manage.py migrate
 python manage.py loaddata currency.json
